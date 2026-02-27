3e5bb52cd6d968b44e711316e482c5042e5ca90d
Francisco Pessoa <franciscomspessoa@gmail.com>
2026-02-27 19:29:02 -0300

    base

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { glob } from "astro/loaders";
+
+import { defineCollection } from "astro:content";
+
+import { z } from "astro/zod";
+
+const posts = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./posts/" }),
+  schema: z.object({
+    title: z.string(),
+    up: z.date().transform((date) => date.toISOString().split("T")[0]),
+    mod: z.date().transform((date) => date.toISOString().split("T")[0]),
+    tags: z.array(z.string()),
+    lang: z.string(),
+  }),
+});
+
+export const collections = { posts };
