dc0ae35688f26c66d4af71b135abf13ca232020a
Francisco Pessoa <franciscomspessoa@gmail.com>
2026-02-28 19:00:21 -0300

    weird i18n

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,9 +9,11 @@ const posts = defineCollection({
   schema: z.object({
     title: z.string(),
     up: z.date().transform((date) => date.toISOString().split("T")[0]),
-    mod: z.optional(z.date().transform((date) => date.toISOString().split("T")[0])),
+    mod: z.optional(
+      z.date().transform((date) => date.toISOString().split("T")[0]),
+    ),
     tags: z.array(z.string()),
-    lang: z.string(),
+    // lang: z.string(),
   }),
 });
 
